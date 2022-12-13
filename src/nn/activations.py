37b970b777f822d0e2b193fe80757e696193a3de
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-12-13 10:35:58 +0530

    added softmax activation with loss

--- src/nn/activations.py
+++ src/nn/activations.py
@@ -1,4 +1,5 @@
 import numpy as np
+from . import losses
 
 class ReLU:
     def forward(self, inputs):
@@ -10,7 +11,7 @@ class ReLU:
         self.dinputs = dvalues.copy()
         self.dinputs[self.inputs <= 0] = 0
 
-class Softmax:
+class SoftmaxPUREACTIVATION:
 
     def forward(self, inputs):
         self.inputs = inputs
@@ -25,3 +26,24 @@ class Softmax:
             single_output = single_output.reshape(-1, 1)
             jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
             self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
+
+class Softmax:
+
+    def __init__(self):
+        self.activation = SoftmaxPUREACTIVATION()
+        self.loss = losses.CategoricalCrossentropy()
+
+    def forward(self, inputs, y_true):
+        self.activation.forward(inputs)
+        self.output = self.activation.output
+        return self.loss.calculate(self.output, y_true)
+
+    def backward(self, dvalues, y_true):
+
+        samples = len(dvalues)
+        if len(y_true.shape) == 2:
+            y_true = np.argmax(y_true, axis=1)
+
+        self.dinputs = dvalues.copy()
+        self.dinputs[range(samples), y_true] -= 1
+        self.dinputs = self.dinputs / samples
