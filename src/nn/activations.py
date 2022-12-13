71bf0b8f6babe0fab676119cfa9dc56c00ca123f
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-12-13 08:34:20 +0530

    updated softmax activation

--- src/nn/activations.py
+++ src/nn/activations.py
@@ -11,12 +11,13 @@ class ReLU:
         self.dinputs[self.inputs <= 0] = 0
 
 class Softmax:
+
     def forward(self, inputs):
         self.inputs = inputs
         exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
         self.output = probabilities
-        return self.output
 
     def backward(self, dvalues):
         self.dinputs = np.empty_like(dvalues)
